5a7d00010c8016a23e1db88d65bbb140ca7c300c
San06-s <santhoshsm0604@gmail.com>
2025-05-12 21:50:34 +0530

    created a calculator program

--- Calculator.py
+++ Calculator.py
@@ -1,1 +1,18 @@
 #calculator
+class calculator():
+    def __init__ (self,a,b,c):
+        self.num1 = a
+        self.num2 = b
+        self.num3 = c
+    def add(self):
+        print(self.num1+self.num2+self.num3)
+    def mul(self):
+        print(self.num1*self.num2*self.num3)
+a=int(input("Enter first number: "))
+b=int(input("Enter second number: "))
+c=int(input("Enter third number: "))
+cal = calculator(a,b,c)
+cal.add()
+cal.mul()
+
+    
