3032b19be27704d500167c0156be8c45c78bd8b9
San06-s <santhoshsm0604@gmail.com>
2025-05-13 10:35:27 +0530

    created a new file brand.py

---
+++ brand.py
@@ -0,0 +1,6 @@
+#brand name generator
+print("Welcome to the Brand Name Generator!")
+city = input("What's the name of the city you grew up in?\n")
+animal = input("What's your fav animal?\n")
+brand_name = city + " " + animal
+print("Your brand name could be " + brand_name)
