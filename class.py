8702870daceaaaec593529d3a31e3802d96bc3fa
San06-s <santhoshsm0604@gmail.com>
2025-05-14 21:10:36 +0530

    New file added with code in it

---
+++ class.py
@@ -0,0 +1,15 @@
+class Mark1:
+    def suit(self):
+        print("M1 is a multipurpose suit")
+class Mark2:
+    def suit(self):
+        print("M2 is a wepanised suit for war")
+class Mark3:
+    def suit(self):
+        print("M3 is a stealth suit")
+M1 = Mark1()
+M2 = Mark2()
+M3 = Mark3()
+M1.suit()
+M2.suit()
+M3.suit()
