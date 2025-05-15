32484d1f4a0b7f65a1e178847dd0d4ca245df834
San06-s <santhoshsm0604@gmail.com>
2025-05-15 13:49:38 +0530

    created a new file logical.py

--- logical.py
+++ logical.py
@@ -1,1 +1,11 @@
 #logical operators
+print("Welcome to the Theme park")
+age = int(input("Enter your age: "))
+if age <= 5:
+    print("You are a infant,Its free for you here!")
+elif age > 5 and age <= 15:
+    print("You are a children,You have to pay Rs.100 for entry")
+elif age > 15 and age <= 50:
+    print("You are an adult,You have to pay Rs.200 for entry")
+
+
